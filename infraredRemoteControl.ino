37c0ed461f9e35fe48fbec3bb205a402fcb001ac
Nick Tag <nick.tagliasacchi@gmail.com>
2017-04-12 02:13:51 -0400

    Moving all strings from SRAM to flash memory

--- infraredRemoteControl.ino
+++ infraredRemoteControl.ino
@@ -36,7 +36,7 @@ IRdecode decoder;
 void setup() {
   Serial.begin(9600);
   receiver.enableIRIn(); // Start receiver
-  Serial.println("SMOKO Unicorn Controller. Type 'H' for help.\n");
+  Serial.println(F("SMOKO Unicorn Controller. Type 'H' for help.\n"));
 }
 
 void loop() {
@@ -50,90 +50,92 @@ void loop() {
   switch (input) {
     case '1':
       sender.send(NEC, BUTTON_ON, 20);
-      Serial.println("Command Executed: Power ON");
+      Serial.println(F("Command Executed: Power ON"));
       break;
     case '0':
       sender.send(NEC, BUTTON_OFF, 20);
-      Serial.println("Command Executed: Power OFF");
+      Serial.println(F("Command Executed: Power OFF"));
       break;
     case '+':
       sender.send(NEC, BRIGHT_UP, 20);
-      Serial.println("Command Executed: Brightness Up");
+      Serial.println(F("Command Executed: Brightness Up"));
       break;
     case '-':
       sender.send(NEC, BRIGHT_DOWN, 20);
-      Serial.println("Command Executed: Brightness Down");
+      Serial.println(F("Command Executed: Brightness Down"));
       break;
     case 'R':
     case 'r':
       sender.send(NEC, COLOR_RED, 20);
-      Serial.println("Command Executed: Lamp Color: RED");
+      Serial.println(F("Command Executed: Lamp Color: RED"));
       break;
     case 'G':
     case 'g':
       sender.send(NEC, COLOR_GREEN, 20);
-      Serial.println("Command Executed: Lamp Color: GREEN");
+      Serial.println(F("Command Executed: Lamp Color: GREEN"));
       break;
     case 'B':
     case 'b':
       sender.send(NEC, COLOR_BLUE, 20);
-      Serial.println("Command Executed: Lamp Color: BLUE");
+      Serial.println(F("Command Executed: Lamp Color: BLUE"));
       break;
     case 'W':
     case 'w':
       sender.send(NEC, COLOR_WHITE, 20);
-      Serial.println("Command Executed: Lamp Color: WHITE");
+      Serial.println(F("Command Executed: Lamp Color: WHITE"));
       break;
     case 'P':
     case 'p':
       sender.send(NEC, COLOR_PURPLE, 20);
-      Serial.println("Command Executed: Lamp Color: PURPLE");
+      Serial.println(F("Command Executed: Lamp Color: PURPLE"));
       break;
     case 'O':
     case 'o':
       sender.send(NEC, COLOR_ORANGE, 20);
-      Serial.println("Command Executed: Lamp Color: ORANGE");
+      Serial.println(F("Command Executed: Lamp Color: ORANGE"));
       break;
     case 'F':
     case 'f':
       sender.send(NEC, BUTTON_FLASH, 20);
-      Serial.println("Command Executed: FLASH");
+      Serial.println(F("Command Executed: FLASH"));
       break;
     case 'T':
     case 't':
       sender.send(NEC, BUTTON_STROBE, 20);
-      Serial.println("Command Executed: STROBE");
+      Serial.println(F("Command Executed: STROBE"));
       break;
     case 'A':
     case 'a':
       sender.send(NEC, BUTTON_FADE, 20);
-      Serial.println("Command Executed: FADE");
+      Serial.println(F("Command Executed: FADE"));
       break;
     case 'S':
     case 's':
       sender.send(NEC, BUTTON_SMOOTH, 20);
-      Serial.println("Command Executed: SMOOTH");
+      Serial.println(F("Command Executed: SMOOTH"));
       break;
     case 'H':
     case 'h':
-      Serial.println("Command Executed: Help Menu");
-      Serial.println("-----------------------");
-      Serial.println("Command\t  Description");
-      Serial.println("   1 \t  Turn lamp On");
-      Serial.println("   0 \t  Turn lamp Off");
-      Serial.println("   + \t  Brightness Up");
-      Serial.println("   - \t  Brightness Down");
-      Serial.println("   R \t  Color: Red");
-      Serial.println("   G \t  Color: GREEN");
-      Serial.println("   B \t  Color: BLUE");
-      Serial.println("   W \t  Color: WHITE");
-      Serial.println("   P \t  Color: PURPLE");
-      Serial.println("   O \t  Color: Orange");
-      Serial.println("   F \t  Transition: Flash");
-      Serial.println("   T \t  Transition: Strobe");
-      Serial.println("   A \t  Transition: Fade");
-      Serial.println("   S \t  Transition: Smooth");
-      Serial.println("-----------------------");
+      Serial.println(F("Command Executed: Help Menu"));
+      Serial.println(F("-----------------------"));
+      Serial.println(F("Command\t  Description"));
+      Serial.println(F("   1 \t  Turn lamp On"));
+      Serial.println(F("   0 \t  Turn lamp Off"));
+      Serial.println(F("   + \t  Brightness Up"));
+      Serial.println(F("   - \t  Brightness Down"));
+      Serial.println(F("   R \t  Color: Red"));
+      Serial.println(F("   G \t  Color: GREEN"));
+      Serial.println(F("   B \t  Color: BLUE"));
+      Serial.println(F("   W \t  Color: WHITE"));
+      Serial.println(F("   P \t  Color: PURPLE"));
+      Serial.println(F("   O \t  Color: Orange"));
+      Serial.println(F("   F \t  Transition: Flash"));
+      Serial.println(F("   T \t  Transition: Strobe"));
+      Serial.println(F("   A \t  Transition: Fade"));
+      Serial.println(F("   S \t  Transition: Smooth"));
+      Serial.println(F("-----------------------"));
+      break;
+    default:
       break;
   }
 }
